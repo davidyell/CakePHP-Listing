9679a6136d448666210126c6b37f55be9b7c27c3
David Yell <dyell@ukwebmedia.com>
2013-04-03 15:34:24 +0100

    First draft of the behaviour with the custom find.

--- Model/Behavior/ListableBehavior.php
+++ Model/Behavior/ListableBehavior.php
@@ -3,12 +3,95 @@
  * Listable behaviour for appending a model to a list of items for a select
  *
  * @author David Yell <neon1024@gmail.com>
+ *
+ * Example usage,
+ * $this->Package->Broadband->Behaviors->load('Listing.Listable', array('relatedModel' => array('name' => 'Provider')));
+ * $broadbands = $this->Package->Broadband->find('listing');
+ *
  */
 
 class ListableBehavior extends ModelBehavior {
 
 /**
- * Changes a list of data into a list of (Provider name) Item
+ * Allows the mapping of preg-compatible regular expressions to public or
+ * private methods in this class, where the array key is a /-delimited regular
+ * expression, and the value is a class method. Similar to the functionality of
+ * the findBy* / findAllBy* magic methods.
+ *
+ * @var array
+ */
+    public $mapMethods = array(
+        '/listing/' => '_findListing'
+    );
+
+/**
+ * The settings for this behaviour
+ *
+ * @var array
+ *
+ * @item mode 'array' or 'string' Control whether you want a string returned
+ *  or an optgroup style array
+ */
+    public $settings = array(
+        'relatedModel' => array(
+                'name' => '',
+                'primaryKey' => 'id',
+                'displayField' => 'name'
+            ),
+        'mode' => 'array'
+    );
+
+/**
+ * Add in the listing find method
+ *
+ * @var array
+ */
+    public $findMethods = array(
+        'listing' => true
+    );
+
+/**
+ * Setup this behavior with the specified configuration settings.
+ *
+ * @param Model $model Model using this behavior
+ * @param array $config Configuration settings for $model
+ * @return void
+ */
+    public function setup(\Model $model, $config = array()) {
+        // Add our custom find method to this model
+        $model->findMethods = Hash::merge($model->findMethods, $this->findMethods);
+
+        // If the behaviour has been configured, merge in the settings
+        if (isset($config['relatedModel'])) {
+            $this->settings = Hash::merge($this->settings, $config);
+        }
+    }
+
+/**
+ *
+ *
+ * @param Model $model
+ * @param type $findMethod
+ * @param type $state
+ * @param type $query
+ * @param type $results
+ * @return array
+ */
+    public function _findListing(Model $model, $findMethod, $state, $query, $results = array()) {
+        if($state == 'before'){
+            $query['fields'] = array($model->primaryKey, $model->displayField);
+            $query['contain'] = array(
+                $this->settings['relatedModel']['name'] => array(
+                    'fields' => array($this->settings['relatedModel']['primaryKey'], $this->settings['relatedModel']['displayField'])
+                )
+            );
+            return $query;
+        }
+        return $results;
+    }
+
+/**
+ * Changes a list of data into a list with extra information
  *
  * @param \Model $model The calling model
  * @param array $results Cake data array
@@ -18,15 +101,23 @@ class ListableBehavior extends ModelBehavior {
     public function afterFind(Model $model, $results, $primary) {
         foreach($results as $row){
             if (isset($row[$model->name])) {
-                $list[$row[$model->name]['id']] = "(".$row['Provider']['name'].") ".$row[$model->name]['name'];
+                if ($this->settings['mode'] == 'string') {
+                    $list[$row[$model->name]['id']] = "(".$row['Provider']['name'].") ".$row[$model->name]['name'];
+                } else if ($this->settings['mode'] == 'array') {
+                    $list[$row['Provider']['name']][$row[$model->name]['id']] = $row[$model->name]['name'];
+                } else {
+                    throw new BadMethodCallException($this->mode.' is not a valid mode.');
+                }
             }
         }
 
-        if (isset($list)) {
+        if (isset($list) && is_array($list)) {
             return $list;
         } else {
             return $results;
         }
     }
+
+
 
 }
--- README.md
+++ README.md
@@ -1,23 +1,16 @@
-CakePHP-Listing
-===============
-
+#CakePHP-Listing
 Model behaviour to append a field into a list for a select box. The idea for this behaviour is that if you have a list of items which belong to another model, it will include that models name.
 
-So we change,
+So we change,  
+
 ```
 array(
   1 => 'First',
   2 => 'Second'
 );
-```
-Into this using string mode
-```
-array(
-  1 => '(Cats) First',
-  2 => '(Dogs) Second'
-);
-```
-or, using array mode
+```  
+..into this using array mode..  
+
 ```
 array(
   'Cats' => array(
@@ -27,4 +20,45 @@ array(
     2 => 'Second'
   )
 );
+```  
+..or into this using string mode  
+
 ```
+array(
+  1 => '(Cats) First',
+  2 => '(Dogs) Second'
+);
+```
+
+..and you should end up with nice option groups in your selects.
+![Select box with optgroup](http://i.imgur.com/QP7BhMl.png)
+
+#Installation
+This is a standard CakePHP plugin, so it will need to extracted or submoduled into your `app/Plugin` folder. I call it `Listing`, so it should live in `app/Plugin/Listing`.
+
+You will need to activate the plugin in your `app/Config/bootstrap.php` using `CakePlugin::load('Listing')`, unless you are already using `CakePlugin::loadAll()`
+
+#Usage
+You can attach to the model using the `$actsAs` array. As you would normally. You **must** include the name of the related model that you want to join to when you configure the behaviour.  
+
+You can also specify the fields that you want to use using, `primaryKey` and `displayField`.  
+
+```
+public $actsAs = array(
+    'Listing.Listable' => array(
+        'relatedModel' => array(
+            'name' => 'Provider',
+            'primaryKey' => 'id', // optional - default shown
+            'displayField' => 'name' // optional - default shown
+        ),
+        'mode' => 'array' // optional - default shown
+    )
+);
+```
+
+[More on Behaviours in the Book](http://book.cakephp.org/2.0/en/models/behaviors.html).
+
+Then in order to attach the extra model, I have implemented a custom find called `listing` which will return the formatted list.  
+
+For example,
+`$broadbands = $this->Package->Broadband->find('listing');`
