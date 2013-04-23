7d163f7b8551a5621d4f1d26d076b46acef9d889
David Yell <dyell@ukwebmedia.com>
2013-04-23 17:36:26 +0100

    Started on writing the tests.

---
+++ Test/Case/Model/Behavior/ListableBehaviorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ListableBehaviorTest
+ *
+ * @author David Yell <neon1024@gmail.com>
+ */
+class ListableBehaviorTest extends CakeTestCase {
+
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        parent::tearDown();
+    }
+
+    public function testCustomFindMethod() {
+        
+    }
+
+}
