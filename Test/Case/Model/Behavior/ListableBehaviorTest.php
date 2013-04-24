ec30376ef28c21e9d50d01c6c2da52824de4cb83
David Yell <dyell@ukwebmedia.com>
2013-04-24 11:29:56 +0100

    Wrote a test for the custom find method

--- Test/Case/Model/Behavior/ListableBehaviorTest.php
+++ Test/Case/Model/Behavior/ListableBehaviorTest.php
@@ -1,10 +1,4 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of ListableBehaviorTest
  *
@@ -12,16 +6,49 @@
  */
 class ListableBehaviorTest extends CakeTestCase {
 
-    public function setUp() {
-        parent::setUp();
+/**
+ * Setup the test and instantiate any models and fixtures that we need
+ */
+    public function setUp(){
+        $this->Model = new Model();
+        $this->Model->useTable = false;
+        $this->Model->primaryKey = 'id';
+        $this->Model->displayField = 'display';
+        $this->Model->Behaviors->attach('Listing.Listable', array(
+                'relatedModelName' => 'RelatedModel',
+                'relatedModelPrimaryKey' => 'id',
+                'relatedModelDisplayField' => 'name'
+            )
+        );
     }
 
-    public function tearDown() {
-        parent::tearDown();
+/**
+ * Make sure that the custom find method appends the conditions
+ */
+    public function testCustomFind() {
+        $findMethod = 'listing';
+        $state = 'before';
+        $query = array();
+
+        $expected = array(
+            'fields' => array('id', 'display'),
+            'contain' => array(
+                'RelatedModel' => array(
+                    'fields' => array('id','name')
+                )
+            )
+        );
+
+        $result = $this->Model->_findListing($state, $query);
+
+        $this->assertEqual($result, $expected);
     }
 
-    public function testCustomFindMethod() {
+    // Make sure the array has the optgroup
+    public function testResultsArrayHasOptGroups() {
         
     }
+
+    // Test to make sure that the array which comes back from a find() is correct
 
 }
